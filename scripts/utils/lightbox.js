fd44bf962ca99e503bf49cf148afb0e7b71fb6f4
sowmaki <solene.tassel@hotmail.com>
2024-11-11 20:08:31 +0100

    creation de lightbox

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,3 +1,13 @@
+export function createLightbox(mediasData) {
+  const img = document.createElement('img');
+  mediasData.forEach(media => {
+    const mediasList = mediasData
+
+  })
+
+}
+
+
 export function displayLightbox() {
 
 }
